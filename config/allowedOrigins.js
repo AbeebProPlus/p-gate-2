87a52599954d3010f86d0c64cd0c8c9b5f0e73d1
abeeb ahmad <mdahlat94@gmail.com>
2024-05-12 22:29:11 +0100

    p-gate-2

---
+++ config/allowedOrigins.js
@@ -0,0 +1,3 @@
+const whiteList = ['http://localhost:3000', 'http://localhost:8080']
+
+module.exports = whiteList
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const bodyParser = require("body-parser");
 const router = require("./routes/routes");
 const paystackWebhookRoute = require('./routes/webhookroutes');
 
 const port = process.env.PORT || 8080;
 
+app.use(cors(corsOptions))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
